ec8ca694a44fb2740982159e86257be27433da8b
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2025-04-20 13:36:42 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    lcdDisplay.lcdClearAll()
+})
 lcdDisplay.lcdInitIIC()
 lcdDisplay.lcdClearAll()
 lcdDisplay.lcdSetBgcolor(0xffffff)
+lcdDisplay.lcdDisplayText("Guess The number 1-9", 1, 20, 10, lcdDisplay.FontSize.Large, 0xFF0000)
+basic.pause(100)
+lcdDisplay.lcdDisplayText("Press A to begin", 2, 60, 60, lcdDisplay.FontSize.Large, 0xFF0000)
 basic.forever(function () {
 	
 })
